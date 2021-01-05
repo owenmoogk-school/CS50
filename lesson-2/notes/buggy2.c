82cd6a0be6d55a080f1cef934359d4f0d917831d
owenmoogk <owenmoogk@gmail.com>
2021-01-05 10:33:41 -0500

    added lesson 2 notes

---
+++ lesson-2/notes/buggy2.c
@@ -0,0 +1,10 @@
+//debugging
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void){
+    for (int i = 0; i < 10; i++){
+        printf("#\n");
+    }
+}
---
+++ lesson-2/notes/exit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(int argc, string argv[]){
+    if (argc != 2){
+        printf("missing cmd line arg\n");
+        return 1;
+    }
+    printf("all is good\n");
+}
